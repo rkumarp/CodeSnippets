360e5b7d3238f2a28059e79c53929eec3448dc65
Ranjit Kumar Parvathaneni <rnjthkmr88@gmail.com>
2013-10-23 02:14:27 -0500

    Update SearchIntInASortedRotatedArray.java

--- SearchIntInASortedRotatedArray.java
+++ SearchIntInASortedRotatedArray.java
@@ -1,13 +1,13 @@
-public int getMinElement(int[] a, size n) {
+public int getMinElement(int[] a, size n)
+{
     int start, mid, end;
     start  = 0;
     end = n - 1
-    while(start < end) {
+    while(start < end)
+    {
         mid = (start + end) / 2;
-
         //pick left
         if(a[start] > a[mid]) end = mid - 1;
-         
         //pick right
         else start = mid + 1
     }
