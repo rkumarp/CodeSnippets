870744c7d69fae529403fdc32e3af643c6a9a2b0
Ranjit Kumar Parvathaneni <rnjthkmr88@gmail.com>
2013-10-23 21:14:07 -0500

    Update BinarySearch.java

--- java/BinarySearch.java
+++ java/BinarySearch.java
@@ -1,31 +1,31 @@
 //ITERATIVE
 
 public int binarySearch(int[] A, int key, int imin, int imax) {
-    int imid;
-    while (imin <= imax) {
-          imid = (imin + imax) / 2;
-          if (a[imid] < key)
-              imax = imid - 1;
-          else if (a[imid] > key)
-               imin = imid + 1;
-          else 
-               return imid;
-    }
-    return -1;
+	int imid;
+	while (imin <= imax) {
+		imid = (imin + imax) / 2;
+		if (a[imid] < key)
+	  		imax = imid - 1;
+		else if (a[imid] > key)
+   			imin = imid + 1;
+		else 
+   			return imid;
+	}
+	return -1;
 }
 
 //RECURSIVE
 
 public int binarySearch(int[] A, int key, int imin, int imax) {
-     int imid;
-     if(imax > imin)
-          return -1;
-     else {
-          if (A[imin] < key)
-               binarySearch(A, key, imin, imid - 1)
-          else if (A[imin] > key)
-               binarySearch(A, key, imid + 1, imax)
-          else
-               return imid;
-     }
+	if(imax > imin)
+		return -1;
+	else {
+		int imid = (imin + imax) / 2;
+		if (A[imin] < key)
+		   	binarySearch(A, key, imin, imid - 1)
+		else if (A[imin] > key)
+			binarySearch(A, key, imid + 1, imax)
+		else
+			return imid;
+	}
 }
