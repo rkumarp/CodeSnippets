b71b06beecb70663af834cd8c3f56079395d834e
Ranjit Kumar Parvathaneni <rnjthkmr88@gmail.com>
2013-10-24 13:29:01 -0500

    Update createAnagrams.java

--- java/createAnagrams.java
+++ java/createAnagrams.java
@@ -9,6 +9,7 @@ public static void anag(String s1, String s2)
 		anag(s1.substring(0, i) + s1.substring(i+1, s1.length()), s2 + s1.charAt(i));
 	}
 }
+
 public static void main(String[] args)
 {
 	anag("tie", "");
