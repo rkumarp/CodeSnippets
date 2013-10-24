885cbea37e884bc7dfa66a5006222adedea7fbe3
Ranjit Kumar Parvathaneni <rnjthkmr88@gmail.com>
2013-10-24 13:27:51 -0500

    Update createAnagrams.java

--- java/createAnagrams.java
+++ java/createAnagrams.java
@@ -1,11 +1,15 @@
-public static void anag(String s1, String s2)	{
-	if(s1.length() == 0)	{
+public static void anag(String s1, String s2)
+{
+	if(s1.length() == 0)
+	{
 		System.out.println(s2);
 	}
-	for(int i = 0; i < s1.length(); i++)	{
+	for(int i = 0; i < s1.length(); i++)
+	{
 		anag(s1.substring(0, i) + s1.substring(i+1, s1.length()), s2 + s1.charAt(i));
 	}
 }
-public static void main(String[] args)	{
+public static void main(String[] args)
+{
 	anag("tie", "");
 }
