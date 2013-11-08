76bf3c1539ed9f515db448c1b4245477031b690c
Ranjit Kumar Parvathaneni <rnjthkmr88@gmail.com>
2013-11-08 00:08:09 -0600

    Create TreeTraversals.java

---
+++ java/TreeTraversals.java
@@ -0,0 +1,49 @@
+public void postOrder(Node root)
+{
+    if(root == null) return;
+    Stack s = new Stack();
+    s.push(root);
+    Node previos;
+    while(!s.isEmty())
+    {
+         Node current = s.peak();
+         
+         //pop when returning back        
+         if((current.left == null && current.right == null) || current.left == previous || current.right == previous)
+         {
+             s.pop();
+             System.out.println(current.data);
+             previous = current;
+             break
+         }
+         //push
+         if(current.left != null)
+             s.push(current.left);
+         if(current.rigth != null)
+             s.push(current.right);
+    }
+}
+
+public void inOrder(Node root)
+{
+    if(root == null) return;
+    Stack s = new Stack();
+    Node current = root;
+    while(true)
+    {    
+        if(current != null)
+        {
+            s.push(current);
+            current = current.left;
+        }
+        else{
+            if(s.isEmpty) break;
+            else
+            {
+                current = s.pop();
+                System.out.println(current.data);
+                current = current.right;
+            }
+        }
+    }
+}
