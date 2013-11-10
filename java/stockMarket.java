b9ebccf35d0306a0df6fe2f6300b6c13eeb099bb
Ranjit Kumar Parvathaneni <rnjthkmr88@gmail.com>
2013-11-09 22:52:22 -0600

    Update stockMarket.java

--- java/stockMarket.java
+++ java/stockMarket.java
@@ -1,27 +1,27 @@
 public int stockMarket(int[] ar)
 {
-    int min_i = 0, max = 0;
+    int min_i = 0, max_profit = 0;
     Pair pair = new Pair(0, 0);
     for(int i = 1; i < ar.length; i++)
     {
         if(ar[i] < ar[min_i])
             min_i = i;
-        if(ar[i] - ar[min_i] > max)
+        if(ar[i] - ar[min_i] > max_profit)
         {
-            pair.min = min_i;
-            pair.max = i;
-            max = ar[i] - ar[min_i];
+            pair.buy = min_i;
+            pair.sell = i;
+            max_profit = ar[i] - ar[min_i];
         }
     }
     return max;
 }
 
 class Pair {
-    int min;
-    int max;
-    public Pair(int min, int max)
+    int buy;
+    int sell;
+    public Pair(int buy, int sell)
     {
-        this.min = min;
-        this.max = max;
+        this.buy = buy;
+        this.sell = sell;
     }
 }
