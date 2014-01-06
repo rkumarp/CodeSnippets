555d3dbbc3f81c261bd8c6e95b6e19f0e1ecfe4c
Ranjit Kumar Parvathaneni <rnjthkmr88@gmail.com>
2014-01-06 11:03:03 -0800

    Create StringSubStringSearch.java

---
+++ java/StringSubStringSearch.java
@@ -0,0 +1,14 @@
+public int search(String haystack, String needle)
+{
+    for(int i = 0; i < haystack.length(); i++)
+    {
+        for(int j = 0; j < needle.length() && i + j < haystack.length(); j++)
+        {
+            if(haystack.charAt(i + j) != needle.charAt(j))
+                break;
+            else if (j == needle.length() - 1)
+                return i;
+        }
+    }
+    return -1;
+}
